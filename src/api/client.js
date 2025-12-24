97a1380068aa180c424a1b8e6fa0fe2d6e1959fa
saianikethreddyp <saianikethreddy@gmail.com>
2025-12-24 20:57:15 +0530

    production api

--- src/api/client.js
+++ src/api/client.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/api',
+    baseURL: import.meta.env.VITE_API_URL || 'https://backend-car-rental-production-a9db.up.railway.app/api',
     headers: {
         'Content-Type': 'application/json'
     }
